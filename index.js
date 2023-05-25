587a6232ea1e7a3e8ad3917946ee7d73f69457fd
Freyanord <annabashina135@gmail.com>
2023-05-25 18:35:31 +0300

    Correct

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const user = document.getElementById('username');
 const avatar = document.getElementById('url-input');
 const comment = document.querySelector('.comment-area');
-const chat = document.querySelector('.chat');
+let chat = document.querySelector('.chat');
 const button = document.querySelector('.button');
 
 //https://i.pinimg.com/280x280_RS/ce/2c/e3/ce2ce3388fb2fd6d3687b41a558d9ad8.jpg
@@ -16,18 +16,22 @@ function capitalize(user){
 };
 
 function checkSpam() {
-    chat.innerHTML = comment.value.replace(/Viagra/i, "***");
+    chat.innerHTML = comment.value.replace(/Viagra/i,"***");
 };
+
+// function checkSpam1() {
+//     chat.innerHTML = comment.value.replace(/xxx/i, "***");
+// };
 
 function addNewMessage () {
     chat.innerHTML = `<div class="container">
     <img src="${avatar.value}" alt="лицо" class="userimage">
     <p class="username">${user.value}</p>
     <p class="message" >${comment.value.replace(/Viagra/i, "***")}</p>
-</div>`};
+</div>`}; 
 
 button.addEventListener('click', addNewMessage);
 
 
-
+/* <p class="message" >${comment.value.replace(/xxx/i, "***")}</p> */
 
